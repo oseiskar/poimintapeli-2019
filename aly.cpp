0d159c395105165ae2841e27a110c5111d91e94b
Otto Seiskari <otto.seiskari@gmail.com>
2018-12-26 21:27:25 +0200

    tsp at endgame

--- aly.cpp
+++ aly.cpp
@@ -18,7 +18,6 @@ void laskeEtaisyydet(int x0, int y0, Lauta<int> &etaisyydet) {
         for (const auto &siirto : siirrot) {
           if (etaisyydet(x,y) < 0 &&
             etaisyydet.torus(x + siirto.dx, y + siirto.dy) == etaisyys - 1) {
-
             yhtaan = true;
             etaisyydet(x,y) = etaisyys;
             break;
@@ -35,7 +34,53 @@ struct Ruutu {
   float arvo;
 };
 
-Ruutu haeKohde(int x, int y, int t, Lauta<char> &kaytetty, const std::vector<Ruutu> &eiTyhjat, const Lauta<int> &lahinVastustaja, int maxSyvyys) {
+void laskeSuoratReitit(int x0, int y0, const Lauta<char> &luvut, const Lauta<int> &lahinVastustaja, Lauta<float> &arvokentta, Lauta<Siirto> &suunnat) {
+  for (int y = 0; y < korkeus; ++y) {
+    for (int x = 0; x < leveys; ++x) {
+      arvokentta(x,y) = -1;
+    }
+  }
+  arvokentta(x0, y0) = 0;
+  int etaisyys = 1;
+  while (true) {
+    bool yhtaan = false;
+    for (int y = 0; y < korkeus; ++y) {
+      for (int x = 0; x < leveys; ++x) {
+        if (arvokentta(x,y) >= 0) continue;
+
+        for (const auto &siirto : siirrot) {
+          if (arvokentta.torus(x + siirto.dx, y + siirto.dy) >= 0) {
+            yhtaan = true;
+            arvokentta(x,y) = -2;
+            break;
+          }
+        }
+      }
+    }
+
+    if (!yhtaan) break;
+
+    for (int y = 0; y < korkeus; ++y) {
+      for (int x = 0; x < leveys; ++x) {
+        if (arvokentta(x,y) >= -1) continue;
+
+        float arvo = -1;
+        for (const auto &siirto : siirrot) {
+          const float a = arvokentta.torus(x + siirto.dx, y + siirto.dy);
+          if (a > arvo) {
+            arvo = a;
+            suunnat(x,y) = siirto;
+          }
+        }
+        const int vastustajanAika = etaisyys - lahinVastustaja(x, y);
+        const float aikakerroin = vastustajanAika > 0 ? std::pow(0.5, vastustajanAika) : 1.0;
+        arvokentta(x,y) = arvo + luvut(x,y) * aikakerroin;
+      }
+    }
+  }
+}
+
+Ruutu haeKohdeTsp(int x, int y, int t, Lauta<char> &kaytetty, const std::vector<Ruutu> &eiTyhjat, const Lauta<int> &lahinVastustaja, int maxSyvyys) {
   assert(maxSyvyys > 0);
 
   const Ruutu *paras = nullptr;
@@ -52,7 +97,7 @@ Ruutu haeKohde(int x, int y, int t, Lauta<char> &kaytetty, const std::vector<Ruu
     float arvo = kohde.arvo * aikakerroin;
     if (maxSyvyys > 1) {
       kaytetty(kohde.x, kohde.y) = 1;
-      arvo += haeKohde(kohde.x, kohde.y, t+etaisyys, kaytetty, eiTyhjat, lahinVastustaja, maxSyvyys-1).arvo;
+      arvo += haeKohdeTsp(kohde.x, kohde.y, t+etaisyys, kaytetty, eiTyhjat, lahinVastustaja, maxSyvyys-1).arvo;
       kaytetty(kohde.x, kohde.y) = 0;
     }
 
@@ -70,10 +115,66 @@ Ruutu haeKohde(int x, int y, int t, Lauta<char> &kaytetty, const std::vector<Ruu
   return { paras->x, paras->y, parasArvo };
 }
 
+float haeArvo(int x, int y, int t, Lauta<char> &luvut, const Lauta<int> &lahinVastustaja, int maxSyvyys) {
+  const char vanha = luvut(x,y);
+  float arvo = 0;
+  constexpr float diskonttauspaino = 0.01;
+  if (maxSyvyys > 0) {
+    luvut(x,y) = 0;
+    for (const auto &siirto : siirrot) {
+      arvo = std::max(arvo, haeArvo(
+        ((x + siirto.dx) + leveys) % leveys,
+        ((y + siirto.dy) + korkeus) % korkeus,
+        t+1, luvut, lahinVastustaja, maxSyvyys-1));
+    }
+    luvut(x,y) = vanha;
+  }
+  float aikakerroin = 1.0 / (1.0 + diskonttauspaino*t);
+  if (vanha > 0) {
+    const int vastustajanAika = t - lahinVastustaja(x, y);
+    if (vastustajanAika > 0) {
+      aikakerroin = 1.0 / (vastustajanAika + 1);
+    }
+  }
+  return arvo + vanha * aikakerroin;
+}
+
+float haeArvo(int omaX, int omaY, const Ruutu &kohde,
+  const Lauta<Siirto> &suunnat, const Lauta<char> &luvut, const Lauta<int> lahinVastustaja,
+  Lauta<char> &hakuCache, int maxSyvyys)
+{
+  hakuCache = luvut;
+  int x = kohde.x, y = kohde.y;
+  int t = 0;
+  while (x != omaX || y != omaY) {
+    hakuCache(x, y) = 0;
+    const Siirto &suunta = suunnat(x, y);
+    x = (x + suunta.dx + leveys) % leveys;
+    y = (y + suunta.dy + korkeus) % korkeus;
+    t++;
+  }
+
+  float arvo = 0;
+  if (maxSyvyys > 0) {
+    for (const auto &siirto : siirrot) {
+      // TODO
+      arvo = std::max(arvo, haeArvo(
+        ((kohde.x + siirto.dx) + leveys) % leveys,
+        ((kohde.y + siirto.dy) + korkeus) % korkeus,
+        t+1, hakuCache, lahinVastustaja, maxSyvyys));
+    }
+  }
+
+  return kohde.arvo + arvo;
+}
+
 struct Toteutus : public Aly {
   std::vector< Lauta<int> > pelaajienEtaisyydet;
+  Lauta<float> arvokentta;
+  Lauta<Siirto> suunnat;
   Lauta<int> lahinVastustaja;
   Lauta<char> kaytetty;
+  Lauta<char> hakuCache;
   std::vector<Ruutu> eiTyhjat;
 
   Toteutus(int nPelaajia) :
@@ -103,6 +204,8 @@ struct Toteutus : public Aly {
       }
     }
 
+    laskeSuoratReitit(omaX, omaY, peli.lauta, lahinVastustaja, arvokentta, suunnat);
+
     eiTyhjat.clear();
     for (int y = 0; y < korkeus; ++y) {
       for (int x = 0; x < leveys; ++x) {
@@ -130,24 +233,53 @@ struct Toteutus : public Aly {
         total *= eiTyhjat.size();
       }
 
-      std::cerr  << "eiTyhjat " << eiTyhjat.size() << ", " << "maxSyvyys " << maxSyvyys << std::endl;
+      if (eiTyhjat.size() < 10) {
+        kohde = haeKohdeTsp(omaX, omaY, 0, kaytetty, eiTyhjat, lahinVastustaja, maxSyvyys);
+      } else {
+        //std::cerr  << "eiTyhjat " << eiTyhjat.size() << ", " << "maxSyvyys " << maxSyvyys << std::endl;
+        //kohde = haeKohde(omaX, omaY, 0, kaytetty, eiTyhjat, lahinVastustaja, maxSyvyys);
 
-      kohde = haeKohde(omaX, omaY, 0, kaytetty, eiTyhjat, lahinVastustaja, maxSyvyys);
+        constexpr int MAX_SYVYYS = 4;
+        float arvo = 0;
+        for (const auto &ruutu : eiTyhjat) {
+          float a = haeArvo(omaX, omaY, ruutu, suunnat, peli.lauta, lahinVastustaja, hakuCache, MAX_SYVYYS);
+          if (a > arvo) {
+            arvo = a;
+            kohde = ruutu;
+          }
+        }
+        //std::cerr << kohde.x << " " << kohde.y << " " << kohde.arvo << std::endl;
+        //tulostaHeuristiikka(hakuCache, std::cerr);
+      }
+
+
+      Ruutu reitti = kohde;
+      char siirto = 'w';
+      while (reitti.x != omaX || reitti.y != omaY) {
+        const Siirto &suunta = suunnat(reitti.x, reitti.y);
+        siirto = suunta.vastamerkki();
+        reitti = {
+          (reitti.x + suunta.dx + leveys) % leveys,
+          (reitti.y + suunta.dy + korkeus) % korkeus,
+          0
+        };
+      }
+      return siirto;
     } else {
       kohde = eiTyhjat[0];
-    }
 
-    const int dx = kohde.x - omaX;
-    const int dy = kohde.y - omaY;
+      const int dx = kohde.x - omaX;
+      const int dy = kohde.y - omaY;
 
-    if (dx > 0) {
-      return 'd';
-    } else if (dx < 0) {
-      return 'a';
-    } else if (dy > 0) {
-      return 's';
-    } else if (dy < 0) {
-      return 'w';
+      if (dx > 0) {
+        return 'd';
+      } else if (dx < 0) {
+        return 'a';
+      } else if (dy > 0) {
+        return 's';
+      } else if (dy < 0) {
+        return 'w';
+      }
     }
 
     assert(false);
